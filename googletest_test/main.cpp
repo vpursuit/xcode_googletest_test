23dde093a078da4bc8111efa4359d631e3c60592
D019141 <peter.trebing@sap.com>
2019-09-04 14:01:17 +0200

    Initial Commit

---
+++ googletest_test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  googletest_test
+//
+//  Created by Trebing, Peter on 04.09.19.
+//  Copyright © 2019 Trebing, Peter. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
