6d2d320baa0937b7603cb9d1a968d4608e37c375
D019141 <peter.trebing@sap.com>
2019-09-04 15:30:32 +0200

    Initial commit

---
+++ Unit Test/Unit_Test.m
@@ -0,0 +1,37 @@
+//
+//  Unit_Test.m
+//  Unit Test
+//
+//  Created by Trebing, Peter on 04.09.19.
+//  Copyright © 2019 Trebing, Peter. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Unit_Test : XCTestCase
+
+@end
+
+@implementation Unit_Test
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
--- googletest_test/main.cpp
+++ googletest_test/main.cpp
@@ -7,9 +7,8 @@
 //
 
 #include <iostream>
+#include <gtest/gtest.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+TEST(GTTest, SimpleAssert){
+    ASSERT_TRUE(true);
 }
